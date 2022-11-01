a43294ad457c10afa8f78d51461c4dc9d5546caf
richard.lee <richard.lee@wizigo.tw>
2022-11-01 09:06:08 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,102 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import io from "socket.io-client";
+import { onBeforeMount, ref } from "vue";
+const socket = io("https://nest-socket-test.herokuapp.com/");
+const messages = ref([]);
+const messageText = ref("");
+const joined = ref(false);
+const name = ref("");
+const typingDisplay = ref("");
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+onBeforeMount(() => {
+  socket.emit("findAllMessages", {}, (response) => {
+    messages.value = response;
+  });
+  socket.on("message", (message) => {
+    messages.value.push(message);
+  });
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  socket.on("typing", ({ name, isTyping }) => {
+    if (isTyping) {
+      typingDisplay.value = `${name} 正在輸入...`;
+    } else {
+      typingDisplay.value = "";
+    }
+  });
+});
+
+const join = () => {
+  socket.emit("join", { name: name.value }, () => {
+    joined.value = true;
+  });
+};
+
+const sendMessage = () => {
+  socket.emit("createMessage", { text: messageText.value }, () => {
+    messageText.value = "";
+  });
+};
+
+
+const emitTyping = () => {
+  socket.emit("typing", { isTyping: true });
+  setTimeout(() => {
+    socket.emit("typing", { isTyping: false });
+  }, 2000);
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<template>
+  <div class="chat">
+    <div v-if="!joined">
+      <form @submit.prevent="join">
+        <label> 你的名字? </label>
+        <input v-model="name" />
+        <button type="submit">送出</button>
+      </form>
+    </div>
+    <div class="chat-container">
+
+      <div class="messages-container">
+        <div v-for="(message,index) in messages" :key="message.text+index">
+          [{{message.name}}]: {{message.text}}
+        </div>
+      </div>
+
+      <div v-if="typingDisplay">{{typingDisplay}}</div>
+
+      <hr />
+
+      <div class="message-input">
+        <form @submit.prevent="sendMessage">
+          <label>訊息:</label>
+          <input
+            v-model="messageText"
+            @input="emitTyping"
+          />
+          <button type="submit">送出</button>
+        </form>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@import "./assets/base.css";
+
+.chat {
+  padding: 20px;
+  height: 100vh;
+}
+
+.chat-container {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.messages-container {
+  flex: 1;
 }
 </style>
